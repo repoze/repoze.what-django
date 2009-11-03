8ddf85434b97dbc1e3d1d763ef2ed448e0eb3745
Gustavo Narea <me@gustavonarea.net>
2009-11-03 15:45:01 +0000

    Initial import of the repoze.what Django plugin.

---
+++ README.txt
@@ -0,0 +1,6 @@
+*********************************************
+The repoze.what plugin for Django integration
+*********************************************
+
+This is an extras plugin for repoze.what which provides optional and handy 
+utilities for Django applications using this authorization framework.
---
+++ setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2009, 2degrees Limited <gustavonarea@2degreesnetwork.com>.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.txt")).read()
+version = open(os.path.join(here, "VERSION.txt")).readline().rstrip()
+
+setup(name="repoze.what-django",
+      version=version,
+      description=("The repoze.what v1 plugin for Django integration"),
+      long_description=README,
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Security"
+        ],
+      keywords="web application wsgi server django authorization repoze",
+      author="Gustavo Narea",
+      author_email='repoze-dev@lists.repoze.org',
+      namespace_packages = ['repoze', 'repoze.what', 'repoze.what.plugins'],
+      url='http://code.gustavonarea.net/repoze.what-django/',
+      license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=[
+          "repoze.what >= 1.0.8",
+          "coverage",
+          "nose",
+          "Django >= 1.1",
+          ],
+      install_requires=[
+          "repoze.what >= 1.0.4",
+          "Django >= 1.1",
+          "decorator >= 3.0",
+          ],
+      test_suite='nose.collector',
+      entry_points = """\
+      """
+      )
